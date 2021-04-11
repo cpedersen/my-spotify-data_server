f54346c4063ad9f5e2fea47989d25c3bb36831e8
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-04-11 16:18:41 -0400

    Change users to spotify_users since 'user' is reserved

--- migrations/001.undo.create_spotify.sql
+++ migrations/001.undo.create_spotify.sql
@@ -2,5 +2,5 @@ DROP TABLE IF EXISTS listening_history;
 DROP TABLE IF EXISTS audio_analysis;
 DROP TABLE IF EXISTS tracks;
 DROP TABLE IF EXISTS playlists;
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS spotify_users;
 
