7b9e0ea8c6322f60e0ba79910c62a27fc2407dd1
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-04-12 19:54:34 -0400

    Updated sql files

--- migrations/001.undo.create_spotify.sql
+++ migrations/001.undo.create_spotify.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS listening_history;
 DROP TABLE IF EXISTS audio_analysis;
-DROP TABLE IF EXISTS playlists_tracks;
 DROP TABLE IF EXISTS tracks;
 DROP TABLE IF EXISTS playlists;
 DROP TABLE IF EXISTS spotify_users;
+DROP TABLE IF EXISTS playlist_tracks;
 
