d738e2b36476e68a96d680b4ab03af6a9f5195af
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-04-19 15:06:14 -0400

    Postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -29,6 +29,10 @@ module.exports = {
   database,
   username,
   password,
+  connectionString:
+    process.env.NODE_ENV === "test"
+      ? process.env.TEST_DATABASE_URL
+      : process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
